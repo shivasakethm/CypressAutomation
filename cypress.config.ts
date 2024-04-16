f4146fa6a5b6e31d5bf6307dc466616fb8a2ba63
shivasakethm <shivasaketh02@gmail.com>
2024-04-15 22:55:14 -0700

    Added Cypress.yml file

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,6 +3,9 @@ import { defineConfig } from 'cypress'
 export default defineConfig({
   e2e: {
     // Configure your E2E tests here
-    specPattern: "cypress/e2e/**/*.{cy,spec}.{js,ts}"
+    specPattern: "cypress/e2e/**/*.{cy,spec}.{js,ts}",
+    projectId: 'vzxc5w'
   },
 })
+
+
